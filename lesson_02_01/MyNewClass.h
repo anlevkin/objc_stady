33788ce35f9e303bbc4a4e0395dc0f0f98e25a44
anlevkin <myregstr@ya.ru>
2017-05-04 12:28:55 +0300

    Создал свой класс в своей ветке

---
+++ lesson_02_01/MyNewClass.h
@@ -0,0 +1,13 @@
+//
+//  MyNewClass.h
+//  lesson_02_01
+//
+//  Created by Антон Левкин on 04.05.17.
+//  Copyright © 2017 Антон Левкин. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyNewClass : NSObject
+
+@end
---
+++ lesson_02_01/MyNewClass.m
@@ -0,0 +1,13 @@
+//
+//  MyNewClass.m
+//  lesson_02_01
+//
+//  Created by Антон Левкин on 04.05.17.
+//  Copyright © 2017 Антон Левкин. All rights reserved.
+//
+
+#import "MyNewClass.h"
+
+@implementation MyNewClass
+
+@end
